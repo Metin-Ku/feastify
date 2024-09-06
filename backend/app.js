637d823a2b5f2b2208c3982e1f83baf9d8c86a42
Metin Kuran <metinkuran98@gmail.com>
2024-09-06 13:16:46 +0300

    6th push

--- backend/app.js
+++ backend/app.js
@@ -71,9 +71,9 @@ if (process.env.NODE_ENV === 'development') {
 if (process.env.NODE_ENV === 'production') {
   console.log("**********************")
   app.use(express.static(path.join(__dirname, '../frontend/dist')));
-  // app.get('*', (req, res) => {
-  //   res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
-  // });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
+  });
 }
 
 // Limit requests from same API
